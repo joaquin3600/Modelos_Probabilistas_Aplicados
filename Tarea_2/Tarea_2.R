3e9d537eccd85b8c9e42e9abcfb11993ee209087
Joaquin <joaquin-dz@hotmail.com>
2020-09-15 02:35:38 -0500

    Limpieza de codigo

--- Tarea_2/Tarea_2.R
+++ Tarea_2/Tarea_2.R
@@ -27,18 +27,19 @@ library('gutenbergr')
         letras        <- Libro %>% unnest_tokens(chars, text, "characters")
         palabras      <- Libro %>% unnest_tokens(words, text, "words")
     
-    #Articulos     <- read_csv("Articles.csv")
-        Articulos     <- elements <- read.csv(file.path( "Filter.csv"))
     #3) Frecuencias de Palabras y letras, ordenados por numero de frecuencia
     
         FrecuenciaPalabras <- as.data.frame( sort(table( palabras$words),decreasing = TRUE))
         FrecuenciaLetras   <- as.data.frame( sort(table( letras$chars)  ,decreasing = TRUE))
     
     #4) Remover numeros de las frecuencias
+        
         FrecuenciaPalabras <- FrecuenciaPalabras[-grep('^\\d+$', FrecuenciaPalabras$Var1),]
         FrecuenciaLetras   <- FrecuenciaLetras  [-grep('^\\d+$', FrecuenciaLetras$Var1),]
+        
     #4.1) Remover articulos y conjunciones de la frecuencia de las palabras
-       
+        
+        Articulos     <- elements <- read.csv(file.path( "Filter.csv"))
         FrecuenciaPalabras = FrecuenciaPalabras[!(FrecuenciaPalabras$Var1 %in% Articulos$Articles),]
         FrecuenciaPalabras = FrecuenciaPalabras[!(FrecuenciaPalabras$Var1 %in% Articulos$Conjunctions),]    
         FrecuenciaPalabras = FrecuenciaPalabras[!(FrecuenciaPalabras$Var1 %in% Articulos$Pronouns),]    
